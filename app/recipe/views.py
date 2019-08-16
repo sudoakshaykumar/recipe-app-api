4d7b2ac3a2932ed49f5a9a5b6b1fcdbb2a18bd06
ajmac1089 <ajmac1089@administrators-MacBook-Pro.local>
2019-08-16 19:05:51 +0530

    Implement feature for filtering tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+
 from core.models import Tag, Ingredient, Recipe
+
+from recipe import serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from recipe import serializers
+from rest_framework import viewsets, mixins, status
 
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
@@ -15,8 +17,15 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        """Return objects for current user"""
+        assigned_only = bool(self.request.query_params.get('assigned_only'))
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create a new ingredient"""
@@ -42,6 +51,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
         tags = self.request.query_params.get('tags')
@@ -53,17 +66,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if ingredients:
             ingredient_ids = self._params_to_ints(ingredients)
             queryset = queryset.filter(ingredients__id__in=ingredient_ids)
-
         return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
-
         elif self.action == 'upload_image':
             return serializers.RecipeImageSerializer
-
         return self.serializer_class
 
     def perform_create(self, serializer):
@@ -90,7 +100,3 @@ class RecipeViewSet(viewsets.ModelViewSet):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
-
-    def _params_to_ints(self, qs):
-        """Convert a list of string IDs to a list of integers"""
-        return [int(str_id) for str_id in qs.split(',')]
